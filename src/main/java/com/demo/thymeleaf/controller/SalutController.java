d30cd9cddc2dd9d1960687298b1b101b14558470
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-06-30 12:30:56 +0200

    avancement structure

--- src/main/java/com/demo/thymeleaf/controller/SalutController.java
+++ src/main/java/com/demo/thymeleaf/controller/SalutController.java
@@ -1,7 +1,9 @@
 package com.demo.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SalutController {
@@ -9,5 +11,11 @@ public class SalutController {
     @GetMapping("/")
     public String salut() {
         return "index";
+    }
+
+    @GetMapping("/salut")
+    public String salutPage(@RequestParam(defaultValue = "Monde") String name, Model model) {
+        model.addAttribute("name", name);
+        return "salut";
     }
 }
